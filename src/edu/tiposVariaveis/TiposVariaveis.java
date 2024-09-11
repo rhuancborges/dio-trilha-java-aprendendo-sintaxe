698b29fd76e53d80d3ce3f95dc148090c6901290
rhuancborges <rhucambor@gmail.com>
2024-09-11 18:21:12 -0300

    tipos variaveis

---
+++ src/edu/tiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,8 @@
+package edu.tiposVariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        float pi = 3.14F; // para float, tem que colocar o F no final
+
+    }
+}
